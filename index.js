ac8788251607c08efe0a38bc50f2b0a993848ebb
David Avery <eismausen@gmail.com>
2021-09-24 14:34:14 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrStrings, eventName) {
+    let returnArr = [];
+    for (let i=0; i<arrStrings.length; i++) {
+      returnArr.push(`Thank you, ${arrStrings[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return returnArr;
+  }
+  
+  function countDown(anyPosInt) {
+    while (anyPosInt >= 0) {
+        console.log(anyPosInt);
+        anyPosInt--;
+    }  
+  }
